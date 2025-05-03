8e672f1a7a8db1119c04d70d123b38db99cf18ca
Atharv Dubey <athdubey120904@gmail.com>
2025-05-03 17:02:11 +0530

    Modularised code

--- converter.py
+++ converter.py
@@ -1,4 +1,4 @@
-def pdiff_handler(custom, iteratorIdx):
+def iteration_core(custom, iteratorIdx):
     degree = ""
     func = ""
     dep_unsanitized = ""
@@ -24,62 +24,45 @@ def pdiff_handler(custom, iteratorIdx):
         degree_cpy = int(degree)
     else:
         degree_cpy = 1
+    dep_list = dep_unsanitized.split(',')
+    
+    return degree, degree_cpy, func, dep_list 
+
+
+
+def pdiff_handler(custom, iteratorIdx):
+    degree, degree_cpy, func, dep_list = iteration_core(custom, iteratorIdx)
     if(degree_cpy>1):
         num_sanitized ="\\frac{\\partial^" + str(degree_cpy) +" "+ func
     else:
         num_sanitized ="\\frac{\\partial " + func 
     num_sanitized+='}'
-    dep_list = dep_unsanitized.split(',')
     dep_sanitized = '{'
     for i in range(len(dep_list)):
         if(i==len(dep_list)-1 and degree_cpy>1):
             dep_sanitized += ' \\partial ' + dep_list[i]+'^'+ str(degree_cpy)
-        elif (i==0):
+        elif (not i):
             dep_sanitized += '\\partial ' + dep_list[i]
         else:
             dep_sanitized += ' \\partial ' + dep_list[i]
         degree_cpy -= 1
     dep_sanitized += '}'
+
     return num_sanitized+dep_sanitized
 
 
 def diff_handler(custom, iteratorIdx):
-    degree = ""
-    func = ""
-    dep_unsanitized = ""
-    if(custom[iteratorIdx-1]=='['):
-        while(custom[iteratorIdx] != ']'):
-            degree+= custom[iteratorIdx]
-            iteratorIdx+=1
-        iteratorIdx += 2
-    openCount = 1
-    while (openCount):
-        if(custom[iteratorIdx] == '('):
-            openCount += 1
-        elif (custom[iteratorIdx] == ')'):
-            openCount -= 1
-        if(openCount):
-            func += custom[iteratorIdx]
-        iteratorIdx += 1
-    iteratorIdx += 1
-    while(custom[iteratorIdx] != ']'):
-        dep_unsanitized+= custom[iteratorIdx]
-        iteratorIdx += 1
-    if(degree != ""):
-        degree_cpy = int(degree)
-    else:
-        degree_cpy = 1
+    degree, degree_cpy, func, dep_list = iteration_core(custom, iteratorIdx)
     if(degree_cpy>1):
         num_sanitized ="\\frac{d^" + str(degree_cpy) +" "+ func
     else:
-        num_sanitized ="\\frac{d " + func 
+        num_sanitized ="\\frac{d" + func 
     num_sanitized+='}'
-    dep_list = dep_unsanitized.split(',')
     dep_sanitized = '{'
     for i in range(len(dep_list)):
         if(i==len(dep_list)-1 and degree_cpy>1):
             dep_sanitized += ' d' + dep_list[i]+'^'+ str(degree_cpy)
-        elif (i==0):
+        elif (not i):
             dep_sanitized += 'd' + dep_list[i]
         else:
             dep_sanitized += ' d' + dep_list[i]
@@ -89,6 +72,7 @@ def diff_handler(custom, iteratorIdx):
 
 
 def integ_handler(custom, iteratorIdx):
+    #under progress , syntax Integ(x)[x]
     return ""
 
 
@@ -115,7 +99,7 @@ def convert(custom):
                 
 
 
-
-syntax = input("enter syntax: ")
-print(convert(syntax))
+while(True):
+    syntax = input("enter syntax: ")
+    print(convert(syntax))
     
