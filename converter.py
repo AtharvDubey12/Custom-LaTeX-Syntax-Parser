7ff7d7800f0a992fc9faf31f398894b7fe30cb20
Atharv Dubey <athdubey120904@gmail.com>
2025-05-03 18:03:47 +0530

    Added Integration (with and without limits)

--- converter.py
+++ converter.py
@@ -24,14 +24,15 @@ def iteration_core(custom, iteratorIdx):
         degree_cpy = int(degree)
     else:
         degree_cpy = 1
-    dep_list = dep_unsanitized.split(',')
     
-    return degree, degree_cpy, func, dep_list 
+    return (degree, degree_cpy, func, dep_unsanitized, iteratorIdx)
 
 
 
 def pdiff_handler(custom, iteratorIdx):
-    degree, degree_cpy, func, dep_list = iteration_core(custom, iteratorIdx)
+    # syntax example: Pdiff[2](x)[x,y]
+    degree, degree_cpy, func, dep_unsanitized,_ = iteration_core(custom, iteratorIdx)
+    dep_list = dep_unsanitized.split(',')
     if(degree_cpy>1):
         num_sanitized ="\\frac{\\partial^" + str(degree_cpy) +" "+ func
     else:
@@ -52,7 +53,9 @@ def pdiff_handler(custom, iteratorIdx):
 
 
 def diff_handler(custom, iteratorIdx):
-    degree, degree_cpy, func, dep_list = iteration_core(custom, iteratorIdx)
+    # syntax example: Diff[2](x)[x,y]
+    degree, degree_cpy, func, dep_unsanitized,_ = iteration_core(custom, iteratorIdx)
+    dep_list = dep_unsanitized.split(',')
     if(degree_cpy>1):
         num_sanitized ="\\frac{d^" + str(degree_cpy) +" "+ func
     else:
@@ -72,9 +75,46 @@ def diff_handler(custom, iteratorIdx):
 
 
 def integ_handler(custom, iteratorIdx):
-    #under progress , syntax Integ(x)[x]
-    return ""
+    # syntax example: Integ[2](x)[x]
+    degree, degree_cpy, func, dep_unsanitized, iteratorIdx = iteration_core(custom, iteratorIdx)
+    iteratorIdx+=1
+    if(iteratorIdx!=len(custom)):
+        return definteg_handler(custom, iteratorIdx,degree_cpy, func, dep_unsanitized)
+    dep_list = dep_unsanitized.split(',')
+    num_sanitized = '\\'
+    for i in range(degree_cpy):
+        num_sanitized+='i'
+    num_sanitized += 'nt ' + func + ' \\, '
+    for object in dep_list:
+        num_sanitized += 'd' + object + '\\,'
+        degree_cpy -= 1
+    while(degree_cpy):
+        num_sanitized+= 'd' + dep_list[-1] + '\\,'
+        degree_cpy -= 1
+    return num_sanitized[:len(num_sanitized)-2]
 
+def definteg_handler(custom, iteratorIdx,degree_cpy, func, dep_unsanitized):
+    # example syntax: integ[3](x)[x,y,z][0->1,0->2,0->3]
+
+    dep_list = dep_unsanitized.split(',')
+    lim_str = ""
+    while(iteratorIdx != len(custom)):
+        if(custom[iteratorIdx] not in "[]"):
+            lim_str+= custom[iteratorIdx]
+        iteratorIdx += 1
+    num_sanitized = ""
+    lims = lim_str.split(',')
+    for i in range(len(lims)):
+        num_sanitized += '\\int_{' + lims[i].split('->')[0] + '}^{' + lims[i].split('->')[1] + '} '
+    num_sanitized+= func + ' '
+    for object in dep_list:
+        num_sanitized+= '\\, d' + object + ' '
+        degree_cpy-=1
+    while(degree_cpy):
+        num_sanitized+= '\\, d' + dep_list[-1] + ' '
+        degree_cpy-=1
+    
+    return num_sanitized
 
 
 def convert(custom):
