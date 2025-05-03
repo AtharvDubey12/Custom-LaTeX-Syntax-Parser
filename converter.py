4f8d3baa966d1717e02cf9656efa80177f25b1f9
Atharv Dubey <athdubey120904@gmail.com>
2025-05-03 14:03:32 +0530

    modularised code

--- converter.py
+++ converter.py
@@ -1,3 +1,97 @@
+def pdiff_handler(custom, iteratorIdx):
+    degree = ""
+    func = ""
+    dep_unsanitized = ""
+    if(custom[iteratorIdx-1]=='['):
+        while(custom[iteratorIdx] != ']'):
+            degree+= custom[iteratorIdx]
+            iteratorIdx+=1
+        iteratorIdx += 2
+    openCount = 1
+    while (openCount):
+        if(custom[iteratorIdx] == '('):
+            openCount += 1
+        elif (custom[iteratorIdx] == ')'):
+            openCount -= 1
+        if(openCount):
+            func += custom[iteratorIdx]
+        iteratorIdx += 1
+    iteratorIdx += 1
+    while(custom[iteratorIdx] != ']'):
+        dep_unsanitized+= custom[iteratorIdx]
+        iteratorIdx += 1
+    if(degree != ""):
+        degree_cpy = int(degree)
+    else:
+        degree_cpy = 1
+    if(degree_cpy>1):
+        num_sanitized ="\\frac{\\partial^" + str(degree_cpy) +" "+ func
+    else:
+        num_sanitized ="\\frac{\\partial " + func 
+    num_sanitized+='}'
+    dep_list = dep_unsanitized.split(',')
+    dep_sanitized = '{'
+    for i in range(len(dep_list)):
+        if(i==len(dep_list)-1 and degree_cpy>1):
+            dep_sanitized += ' \\partial ' + dep_list[i]+'^'+ str(degree_cpy)
+        elif (i==0):
+            dep_sanitized += '\\partial ' + dep_list[i]
+        else:
+            dep_sanitized += ' \\partial ' + dep_list[i]
+        degree_cpy -= 1
+    dep_sanitized += '}'
+    return num_sanitized+dep_sanitized
+
+
+def diff_handler(custom, iteratorIdx):
+    degree = ""
+    func = ""
+    dep_unsanitized = ""
+    if(custom[iteratorIdx-1]=='['):
+        while(custom[iteratorIdx] != ']'):
+            degree+= custom[iteratorIdx]
+            iteratorIdx+=1
+        iteratorIdx += 2
+    openCount = 1
+    while (openCount):
+        if(custom[iteratorIdx] == '('):
+            openCount += 1
+        elif (custom[iteratorIdx] == ')'):
+            openCount -= 1
+        if(openCount):
+            func += custom[iteratorIdx]
+        iteratorIdx += 1
+    iteratorIdx += 1
+    while(custom[iteratorIdx] != ']'):
+        dep_unsanitized+= custom[iteratorIdx]
+        iteratorIdx += 1
+    if(degree != ""):
+        degree_cpy = int(degree)
+    else:
+        degree_cpy = 1
+    if(degree_cpy>1):
+        num_sanitized ="\\frac{d^" + str(degree_cpy) +" "+ func
+    else:
+        num_sanitized ="\\frac{d " + func 
+    num_sanitized+='}'
+    dep_list = dep_unsanitized.split(',')
+    dep_sanitized = '{'
+    for i in range(len(dep_list)):
+        if(i==len(dep_list)-1 and degree_cpy>1):
+            dep_sanitized += ' d' + dep_list[i]+'^'+ str(degree_cpy)
+        elif (i==0):
+            dep_sanitized += 'd' + dep_list[i]
+        else:
+            dep_sanitized += ' d' + dep_list[i]
+        degree_cpy -= 1
+    dep_sanitized += '}'
+    return num_sanitized+dep_sanitized
+
+
+
+
+
+
 def convert(custom):
     if(not len(custom) or custom[0].upper() != custom[0]):
         return "invalid syntax"
@@ -8,51 +102,10 @@ def convert(custom):
         iteratorIdx+=1
     iteratorIdx+=1
 
-    # Pdiff works well!
-
     if(keyword=='Pdiff'):
-        degree = ""
-        func = ""
-        dep_unsanitized = ""
-        if(custom[iteratorIdx-1]=='['):
-            while(custom[iteratorIdx] != ']'):
-                degree+= custom[iteratorIdx]
-                iteratorIdx+=1
-            iteratorIdx += 2
-        openCount = 1
-        while (openCount):
-            if(custom[iteratorIdx] == '('):
-                openCount += 1
-            elif (custom[iteratorIdx] == ')'):
-                openCount -= 1
-            if(openCount):
-                func += custom[iteratorIdx]
-            iteratorIdx += 1
-        iteratorIdx += 1
-        while(custom[iteratorIdx] != ']'):
-            dep_unsanitized+= custom[iteratorIdx]
-            iteratorIdx += 1
-        if(degree != ""):
-            degree_cpy = int(degree)
-        else:
-            degree_cpy = 1
-        if(degree_cpy>1):
-            num_sanitized ="\\frac{\\partial^" + str(degree_cpy) +" "+ func
-        else:
-            num_sanitized ="\\frac{\\partial " + func 
-        num_sanitized+='}'
-        dep_list = dep_unsanitized.split(',')
-        dep_sanitized = '{'
-        for i in range(len(dep_list)):
-            if(i==len(dep_list)-1 and degree_cpy>1):
-                dep_sanitized += ' \\partial ' + dep_list[i]+'^'+ str(degree_cpy)
-            elif (i==0):
-                dep_sanitized += '\\partial ' + dep_list[i]
-            else:
-                dep_sanitized += ' \\partial ' + dep_list[i]
-            degree_cpy -= 1
-        dep_sanitized += '}'
-        return num_sanitized+dep_sanitized
+        return pdiff_handler(custom, iteratorIdx)
+    elif(keyword=='Diff'):
+        return diff_handler(custom, iteratorIdx)
             
 
                 
